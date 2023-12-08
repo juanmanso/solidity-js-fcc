e8a4feadbed2a26e653eb90064f4c0a9f9941e8b
juam <juanmanso@gmail.com>
2023-12-08 19:41:24 +0100

    feat(L03): add storage factory solidity code used on remix

---
+++ L03-remix-storage-factory/README.md
@@ -0,0 +1,5 @@
+## Remix - Storage Factory
+
+On this lesson we learned how to create a contract that creates other contracts. We also learned how to use the `override` keyword to overload a contract's function to create a new implementation (for example add a bias/offset of 5 in this example).
+
+<img width="1439" alt="image" src="https://github.com/juanmanso/solidity-js-fcc/assets/21087992/ef2d95c6-fe64-44fe-86eb-7e05e2b5950e">
---
+++ L03-remix-storage-factory/SimpleStorage.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+contract SimpleStorage {
+    // // boolean, uint, int, address, bytes, string
+    // bool hasFavoriteNumber = true;
+    // uint256 favoriteNumber = 5;
+    // string favoriteNumberInText = "Five";
+    // int256 favoriteInt = -5;
+    // address myAddress = 0x0794da2794D42E683870AcABD12EdB36B9F3529b;
+    // bytes32 favoriteBytes = "cat";
+
+    // Gets initialized to zero
+    // if not specified, it's assigned aas internal
+    uint256 favoriteNumber;
+
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    // Dynamic size array
+    People[] public people;
+
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
+
+    // view -> no gas because disallow change state (except called by another non-view/pure fx
+    // pure -> no gas because doesn't read the blockchain
+    function retrieve() public view returns (uint256) {
+        return favoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+}
